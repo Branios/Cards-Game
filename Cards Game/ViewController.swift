064610f8b46518ec3ee261f3e28033656f4873b8
Vadim <mr.kavy@yandex.com>
2022-07-29 21:17:47 +0300

    app logic and styles

--- Cards Game/ViewController.swift
+++ Cards Game/ViewController.swift
@@ -8,11 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    
+    @IBOutlet var cardsCollection: [UIButton]!
+    @IBOutlet var tapsLabel: UILabel!
+    
+    let emojiCollections = ["🥃", "🍶", "🍾", "🫖", "🍼", "☕️", "🍸", "🍺", "🧃"]
+    
+    var taps = 0 {
+        didSet {
+            tapsLabel.text = "Taps: (\taps)"
+        }
     }
+    
+    @IBAction func tapTheBtn(_ sender: Any) {
+        taps += 1
+    }
+    
+    private func tapTheCard(emoji: String, button: UIButton) {
+        if button.currentTitle == emoji {
+            //button.backgroundColor = #121212
+            button.setTitle("", for: .normal)
+        } else {
+            //button.backgroundColor = 
+        }
+    }
+    
 
 
 }
